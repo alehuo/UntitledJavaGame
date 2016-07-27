13ca71a8aa18246a9c51610087aa4c3f656c89c9
alehuo <aleksi.huotala@helsinki.fi>
2016-07-27 23:36:33 +0300

    WIP: Collision code progress

--- src/ahuotala/entities/GameObject.java
+++ src/ahuotala/entities/GameObject.java
@@ -5,10 +5,88 @@
  */
 package ahuotala.entities;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 /**
  *
- * @author Aleksi
+ * @author Aleksi Huotala
  */
 public class GameObject {
-    
+
+    private final int x;
+    private final int y;
+    private final int radiusX = 5;
+    private final int radiusY = 5;
+    private final BufferedImage sprite;
+
+    public GameObject(int x, int y, BufferedImage sprite) {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+    }
+
+    public boolean collidesWithPlayer(Player player) {
+        if (player.getX() == x - radiusX && player.getY() >= y - radiusY && player.getY() <= y + sprite.getHeight() + radiusY) {
+            //Left side
+            return true;
+        } else if (player.getX() == x + sprite.getWidth() - radiusX && player.getY() >= y - radiusY && player.getY() <= y + sprite.getHeight() + radiusY) {
+            //Right side
+            return true;
+        } else if (player.getX() >= x - radiusX && player.getX() <= x + sprite.getWidth() + radiusX && player.getY() == y + sprite.getHeight()) {
+            //Bottom side
+            return true;
+        } else if (player.getX() >= x - radiusX && player.getX() <= x + sprite.getWidth() + radiusX && player.getY() == y - radiusY) {
+            //Top side
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void collide(Player player) {
+        if (player.getX() == x - radiusX && player.getY() >= y - radiusY && player.getY() <= y + sprite.getHeight() + radiusY) {
+            //Left side
+            player.setDirectionState(Direction.RIGHT, false);
+            player.setDirectionState(Direction.UP, true);
+            player.setDirectionState(Direction.DOWN, true);
+            player.setDirectionState(Direction.LEFT, true);
+        } else if (player.getX() == x + sprite.getWidth() - radiusX && player.getY() >= y - radiusY && player.getY() <= y + sprite.getHeight() + radiusY) {
+            //Right side
+            player.setDirectionState(Direction.UP, true);
+            player.setDirectionState(Direction.DOWN, true);
+            player.setDirectionState(Direction.LEFT, false);
+            player.setDirectionState(Direction.RIGHT, true);
+        } else if (player.getX() >= x - radiusX && player.getX() <= x + sprite.getWidth() + radiusX && player.getY() == y + sprite.getHeight()) {
+            //Bottom side
+            player.setDirectionState(Direction.UP, false);
+            player.setDirectionState(Direction.DOWN, true);
+            player.setDirectionState(Direction.LEFT, true);
+            player.setDirectionState(Direction.RIGHT, true);
+        } else if (player.getX() >= x - radiusX && player.getX() <= x + sprite.getWidth() + radiusX && player.getY() == y - radiusY) {
+            //Top side
+            player.setDirectionState(Direction.UP, true);
+            player.setDirectionState(Direction.DOWN, false);
+            player.setDirectionState(Direction.LEFT, true);
+            player.setDirectionState(Direction.RIGHT, true);
+        }
+    }
+
+    public void render(Graphics g, Player p) {
+        g.drawImage(sprite, x + p.getOffsetX(), y + p.getOffsetY(), null);
+        if (this.collidesWithPlayer(p)) {
+            this.collide(p);
+        } else {
+            p.setDirectionState(Direction.UP, true);
+            p.setDirectionState(Direction.DOWN, true);
+            p.setDirectionState(Direction.LEFT, true);
+            p.setDirectionState(Direction.RIGHT, true);
+        }
+    }
+
+    public void drawBoundaries(Graphics g, Player p) {
+        g.setColor(Color.MAGENTA);
+        g.draw3DRect(x + p.getOffsetX() - radiusX, y + p.getOffsetY() - radiusY, sprite.getWidth() + 2 * radiusX, sprite.getHeight() + 2 * radiusY, false);
+    }
 }

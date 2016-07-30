93e69e294a79abf8658f59478e825c618ddec6bf
alehuo <aleksi.huotala@helsinki.fi>
2016-07-30 11:44:00 +0300

    Cleanup

--- src/ahuotala/tools/MapGenerator.java
+++ src/ahuotala/tools/MapGenerator.java
@@ -18,14 +18,14 @@ public class MapGenerator {
 
     public static void main(String[] args) {
         try {
-            FileWriter fw = new FileWriter("src/ahuotala/map/map2.map");
+            FileWriter fw = new FileWriter("src/ahuotala/map/map3.map");
             String tmp = "";
-            fw.write("# x,y,z,tile_type,animation\r\n");
-            fw.write("# Map 2\r\n");
+            fw.write("# x,y,tile_type_bottom,tile_type_mask_1,tile_type_mask2,tile_type_fringe_1,tile_type_fringe_2\r\n");
+            fw.write("# Map 3\r\n");
             //Center x = 20, y = 12 (320, 192)
             for (int x = -44; x <= 84; x++) {
                 for (int y = -52; y <= 76; y++) {
-                    fw.write(x * BLOCKSIZE + "," + y * BLOCKSIZE + ",0,grass,0\r\n");
+                    fw.write(x * BLOCKSIZE + "," + y * BLOCKSIZE + ",grass,,,,\r\n");
                 }
             }
             fw.close();

4a1f9130a83a4911c591b40688768b330003b822
alehuo <aleksi.huotala@helsinki.fi>
2016-10-15 22:28:47 +0300

    Add debug class

---
+++ src/ahuotala/game/Debug.java
@@ -0,0 +1,12 @@
+package ahuotala.game;
+
+/**
+ *
+ * @author Aleksi Huotala
+ */
+public class Debug {
+
+    public static void log(String message) {
+        System.out.println("# " + message);
+    }
+}

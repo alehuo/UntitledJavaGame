cb9e311a42c0e229ea29621e8f8f241df93ff313
alehuo <aleksi.huotala@helsinki.fi>
2016-10-15 17:47:07 +0300

    Cleanup

--- src/ahuotala/game/Game.java
+++ src/ahuotala/game/Game.java
@@ -15,7 +15,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Scanner;
 import javax.swing.JFrame;
 
 /**
